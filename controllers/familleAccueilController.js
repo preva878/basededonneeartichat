aceb452d6750c0467719473df0eda0385c022f5a
preva878 <prevotadrian@gmail.com>
2022-05-24 09:47:26 +0200

    commit

--- controllers/familleAccueilController.js
+++ controllers/familleAccueilController.js
@@ -1,3 +1,4 @@
+const res = require("express/lib/response")
 const db = require ("../models/db")
 
 const familleAccueilController = {
@@ -14,6 +15,74 @@ const familleAccueilController = {
             res.end()
         })
     },
+    insertFamilleAccueil(){
+        db.FamilleAccueil.create({
+            Nom:Nom,
+            Adresse:Adresse,
+            Cp:Cp,
+            Ville:Ville,
+            EquipementsFourni:EquipementsFourni,
+            Artichats:Artichats,
+            Notes:Notes
+        })
+        .then(()=>{
+            res.write(JSON.stringify({message:`famille d'accueil ${Nom} ajouter a l'equipe`}))
+            res.end()
+        })
+        .catch(()=>{
+            res.write(JSON.stringify({message:"erreur dans l insertion de famille accueil"}))
+            res.end()
+        })
+    },
+    deleteFamilleAccueil(){
+        db.FamilleAccueil.destroy({
+            where:{
+                id:id
+            },
+        })
+        .then(()=>{
+            res.write(JSON.stringify({message:`famille d'accueil supprimer`}))
+            res.end()
+        })
+        .catch(()=>{
+            res.write(JSON.stringify({message:"erreur"}))
+            res.end()
+        })
+    },
+    getfamilleAccueilbyNom(res,Nom){
+        db.FamilleAccueil.findOne({where:{Nom:Nom}})
+        .then((result)=> {
+            res.write(JSON.stringify(result))
+            res.end()
+        })
+        .catch(()=>{
+            res.write(JSON.stringify({message:`erreur dans la recherche par nom:${Nom}`}))
+            res.end()
+        })
+    },
+    getFamilleAccueilbyArtichat(res,Artichats){
+        db.FamilleAccueil.findOne({where:{Artichats:Artichats}})
+        .then((result) => {
+            res.write(JSON.stringify(result))
+            res.end()
+        })
+        .catch(()=>{
+            res.write(JSON.stringify({message:`erreur dans la recherche pour ${Artichats}`}))
+            res.end()
+        })
+    },
+    getFamilleAccueilbyCP(res,Cp){
+        db.FamilleAccueil.findOne({where:{Cp:Cp}})
+        .then((result)=>{
+            res.write(JSON.stringify(result))
+            res.end()
+        })
+        .catch(()=>{
+            res.write(JSON.stringify({message:`erreur dans la recherche par cp : ${Cp}`}))
+            res.end()
+        })
+    },
+
 }
 
 module.exports = familleAccueilController
--- controllers/sponsorController.js
+++ controllers/sponsorController.js
@@ -1,8 +1,67 @@
+const res = require("express/lib/response")
 const db = require ("../models/db")
 
 const sponsorController = {
     getAllSponsors(){
      db.Sponsor.findAll()
+    },
+    insertSponsors(){
+        db.Sponsor.create({
+            Nom:Nom,
+            Materiel:Materiel,
+            DateEntree:DateEntree,
+            Quantite:Quantite,
+            Types:Types,
+            Adresse:Adresse,
+            Cp:Cp,
+            Ville:Ville,
+        })
+        .then(()=>{
+            removeEventListener.write(JSON.stringify({message:`nouveau sponsor : ${Nom} inserer`}))
+            res.end()
+        })
+        .catch
+        (()=>{
+            res.write(JSON.stringify({message:"erreur d'encodage"}))
+            res.end()
+        })
+    },
+    deleteSponsor(res,id){
+        db.AnimalOut.destroy({
+            where:{
+                id:id
+            },
+        })
+        .then(()=>{
+            res.write(JSON.stringify({message:`sponsor ${Nom} effacer`}))
+            res.end()
+        })
+        .catch(()=>{
+            res.write(JSON.stringify({message:"erreur dans la suppression du sponsor"}))
+            res.end()
+        })
+    },
+    getSponsorByNom(res,Nom){
+        db.Sponsor.findOne({where:{Nom:Nom}})
+        .then((result)=> {
+            res.write(JSON.stringify(result))
+            res.end()
+        })
+        .catch(()=>{
+            res.write(JSON.stringify({message:"erreur dans la recherche par nom"}))
+            res.end()
+        })
+    },
+    getSponsorByCp(res,Cp){
+        db.Sponsor.findOne({where:{Cp:Cp}})
+        .then((result)=> {
+            res.write(JSON.stringify(result))
+            res.end()
+        })
+        .catch(()=>{
+            res.write(JSON.stringify({message:`erreur dans la recherche par CP`}))
+            res.end()
+        })
     },
 }
 
