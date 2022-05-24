628ec796e4786331bae88a880a1455b135d11884
preva878 <prevotadrian@gmail.com>
2022-05-24 10:08:16 +0200

    commit

--- controllers/equipementController.js
+++ controllers/equipementController.js
@@ -1,3 +1,4 @@
+const res = require("express/lib/response")
 const db = require ("../models/db")
 
 const equipementController = {
@@ -32,7 +33,46 @@ const equipementController = {
             res.write(JSON.stringify({message:"erreur dans l insertion"}))
             res.end()
         })
-    }
+    },
+    deleteEquipement(){
+        db.Equipement.destroy({
+            where:{
+                id:id
+            },
+        })
+        .then(()=> {
+            res.write(JSON.stringify({message:`equipement : ${id} supprimer`}))
+            res.end()
+        })
+        .catch(()=>{
+            res.write(JSON.stringify({message:"erreur dans le delete"}))
+            res.end()
+        })
+    },
+    getEquipementbyEtats(res,Etats){
+        db.Equipement.findOne({where:{Etats:Etats}})
+        .then((result)=>{
+            res.write(JSON.stringify(result))
+            res.end()
+        })
+        .catch(()=>{
+            res.write(JSON.stringify({message:`erreur dans la recherche par Etats`}))
+            res.end()
+        })
+    },
+    getEquipementByNom(res,Nom){
+        db.Equipement.findOne({where:{Nom:Nom}})
+        .then((result)=>{
+            res.write(JSON.stringify(result))
+            res.end()
+        })
+        .catch(()=> {
+            res.write(JSON.stringify({message:`erreur dans la recher par nom : ${Nom}`}))
+            res.end()
+        })
+
+        },
+    
 }
 
 module.exports = equipementController
